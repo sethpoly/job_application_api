66a6e9e892d344f21ae012b477c80b5af8f33d00
sethpoly <emailspolyniak@gmail.com>
2021-05-17 17:27:43 -0400

    Updated app.js for cors and linked api

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
 const app = express();
 
 // Used for postman testing
 const routes = require('./routes/api/books');
 app.use('/api/books', routes);
 
+// Routes
+const books = require('./routes/api/books');
+
 // Connect to database
 connectDB();
 
+// Cors
+app.use(cors({ origin: true, credentials: true}));
+
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get('/', (req, res) => res.send('Hello world!'));
+
+// Use routes
+app.use('/api/books', books);
 
 const port = process.env.PORT ||  8082;
 
